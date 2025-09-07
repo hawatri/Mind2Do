60fa61941b3b9be2578deacdc81a82d6555a0740
Hawatri Kia <63999220+hawatri@users.noreply.github.com>
2025-09-07 11:04:03 +0530

    Updated ThemeToggle.tsx

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -9,7 +9,7 @@ export const ThemeToggle: React.FC = () => {
 
   return (
     <>
-      <div className="fixed top-6 right-6 z-50 flex gap-3">
+      <div className="fixed bottom-6 right-6 z-50 flex gap-3">
         {/* Settings Button */}
         <button
           onClick={() => setIsSettingsOpen(true)}
